675698bea12b5a0cd17ac79517f12e2682e392b3
Will Fang <wfang@Wills-MacBook-Pro-Retina.local>
2017-06-08 16:03:53 -0400

    Set up POST and GET superglobals with simple form validation

--- Post_Get_Superglobals_Form/index.php
+++ Post_Get_Superglobals_Form/index.php
@@ -8,19 +8,26 @@
 
 	<body>
 		<div class="w3-card w3-margin w3-padding">
+
+			<?php if(isset($_GET['error'])) : ?>
+				<div class="w3-panel w3-red">
+					<p><?php echo $_GET['error']; ?></p>
+				</div>
+			<?php endif; ?>
+
 			<h2>Register</h2>
-			<form class="w3-container">
+			<form method="POST" action="register.php" class="w3-container">
 				<label class="w3-label w3-text-blue">First Name</label>
-				<input type="text" class="w3-input w3-border">
+				<input type="text" name="first_name" class="w3-input w3-border">
 				<br>
 				<label class="w3-label w3-text-blue">Last Name</label>
-				<input type="text" class="w3-input w3-border">
+				<input type="text" name="last_name" class="w3-input w3-border">
 				<br>
 				<label class="w3-label w3-text-blue">Email</label>
-				<input type="text" class="w3-input w3-border">
+				<input type="text" name="email" class="w3-input w3-border">
 				<br>
 				<label class="w3-label w3-text-blue">Location</label>
-				<select name="" id="" class="w3-input w3-border">
+				<select name="" name="location" class="w3-input w3-border">
 					<option value="Location 1">Location 1</option>
 					<option value="Location 2">Location 2</option>
 					<option value="Location 3">Location 3</option>
