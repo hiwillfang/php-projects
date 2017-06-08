038658471334dec13d1e4a5c6b9e16c432392f83
Will Fang <wfang@Wills-MacBook-Pro-Retina.local>
2017-06-08 11:04:15 -0400

    Scaffold registration form

---
+++ Post_Get_Superglobals_Form/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Registration Form</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	</head>
+
+	<body>
+		<div class="w3-card w3-margin w3-padding">
+			<h2>Register</h2>
+			<form class="w3-container">
+				<label class="w3-label w3-text-blue">First Name</label>
+				<input type="text" class="w3-input w3-border">
+				<br>
+				<label class="w3-label w3-text-blue">Last Name</label>
+				<input type="text" class="w3-input w3-border">
+				<br>
+				<label class="w3-label w3-text-blue">Email</label>
+				<input type="text" class="w3-input w3-border">
+				<br>
+				<label class="w3-label w3-text-blue">Location</label>
+				<select name="" id="" class="w3-input w3-border">
+					<option value="Location 1">Location 1</option>
+					<option value="Location 2">Location 2</option>
+					<option value="Location 3">Location 3</option>
+				</select>
+				<br>
+				<input type="submit" value="Submit">
+			</form>
+		</div>
+	</body>
+</html>
