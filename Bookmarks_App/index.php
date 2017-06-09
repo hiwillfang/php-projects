d54ba3fe6755c9a52c2cf53e06887fce58ab5b99
Will Fang <wfang@Wills-MacBook-Pro-Retina.local>
2017-06-09 11:09:16 -0400

    Update README file

---
+++ Bookmarks_App/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bookmarks App</title>
+		<link rel="stylesheet" href="https://bootswatch.com/slate/bootstrap.min.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+		    <div class="container">
+		        <div class="navbar-header">
+		        	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			            <span class="sr-only">Toggle navigation</span>
+			            <span class="icon-bar"></span>
+			            <span class="icon-bar"></span>
+			            <span class="icon-bar"></span>
+		          	</button>
+		          	<a class="navbar-brand" href="#">Bookmarks App</a>
+		        </div>
+		        <div id="navbar" class="collapse navbar-collapse">
+		        	<ul class="nav navbar-nav">
+		            	<li><a href="index.php">Home</a></li>
+		          	</ul>
+		          	<ul class="nav navbar-nav navbar-right">
+		            	<li><a href="index.php?action=clear">Clear All</a></li>
+		          	</ul>
+		        </div><!--/.nav-collapse -->
+		    </div>
+		</nav>
+		<!-- NAVIGATION END -->
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7">
+					
+				</div>
+				<div class="col-md-5">
+					
+				</div>
+			</div>
+		</div>
+		<!-- CONTENT END -->
+	</body>
+</html>
